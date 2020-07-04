5853db6ba6f81d4e7765fd9a9b068c6291d9c89e
TayWolf <alex.amanz.95@gmail.com>
2020-07-04 13:07:39 -0500

    Animación de un cohete hecho con JS y CSS3

---
+++ JS/Flying Rocket Animation/js/scripts.js
@@ -0,0 +1,22 @@
+function stars(){
+	let count = 25;
+	let scene = document.querySelector('.scene');
+	let i = 0;
+	while(i < count){
+		let star = document.createElement('i');
+		let x = Math.floor(Math.random() * window.innerWidth);
+
+		let duration = Math.random() * 1;
+		let h = Math.random() * 100;
+
+		star.style.left = x + 'px';
+		star.style.width = 1 + 'px';
+		star.style.height = 50 + h + 'px';
+		star.style.animationDuration = duration + 's';
+
+		scene.appendChild(star);
+		i++;
+	}
+}
+
+stars();
